2fefc2968632101b503f0cff4e692c2c585ac62f
Pratik-Guha <142882560+Pratik-Guha@users.noreply.github.com>
2023-08-24 21:54:19 +0530

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,47 @@
+const ApiKey="07a17aacca627c89929ea4559bfffb66";
+const apiurl= "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox=document.querySelector(".search input")
+const searchBtn=document.querySelector(".search button")
+
+const weatherIcon=document.querySelector(".weather-icon")
+async function checkWeather(city){
+	const response=await fetch(apiurl + city+`&appid=${ApiKey}`);
+	if(response.status==404){
+		document.querySelector(".error").style.display="block"
+		document.querySelector(".weather").style.display="none"
+	}
+	else{
+		document.querySelector(".error").style.display="none"
+	}
+
+	let data=await response.json();
+	
+	document.querySelector(".city").innerHTML=data.name;
+	document.querySelector(".temp").innerHTML=Math.round(data.main.temp) +"℃";
+	document.querySelector(".humidity").innerHTML=data.main.humidity+"%";
+	document.querySelector(".wind").innerHTML=data.wind.speed+"km/h";
+
+	if(data.weather[0].main=="Clouds" ){
+		weatherIcon.src="weather-app-img/images/clouds.png";
+	}
+	else if(data.weather[0].main=="Clear"){
+		weatherIcon.src="weather-app-img/images/clear.png";
+	}
+	else if(data.weather[0].main=="Rain"){
+		weatherIcon.src="weather-app-img/images/rain.png";
+	}
+	else if(data.weather[0].main=="Snow" ||Math.round(data.main.temp)<15+"℃"){
+		weatherIcon.src="weather-app-img/images/snow.png";
+	}
+	else if(data.weather[0].main=="Drizzle"){
+		weatherIcon.src="weather-app-img/images/drizzle.png";
+	}
+	document.querySelector(".weather").style.display="block";
+	
+};
+
+searchBtn.addEventListener("click",() =>{
+
+	checkWeather(searchBox.value);
+})
